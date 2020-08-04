be5fbfb890c556939b1c690c80c4a105d27a4099
hariyadav-08 <hariyaadav143@gmail.com>
2020-08-04 12:06:27 +0530

    addedfiles

---
+++ src/devopsapp/Ilp1.java
@@ -0,0 +1,14 @@
+package devopsapp;
+
+public class Ilp1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" welocme to devops ");
+		System.out.println(" Devops professionnal certification ");
+		
+		
+
+	}
+
+}
